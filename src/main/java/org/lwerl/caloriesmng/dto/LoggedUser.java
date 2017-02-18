af95227a6e25133febde83a75a049748bc8c7213
lWeRl <werasdas@gmail.com>
2017-02-18 11:44:41 +0300

    Изменение для работы ThreadSafe

--- src/main/java/org/lwerl/caloriesmng/dto/LoggedUser.java
+++ src/main/java/org/lwerl/caloriesmng/dto/LoggedUser.java
@@ -2,14 +2,28 @@ package org.lwerl.caloriesmng.dto;
 
 import org.lwerl.caloriesmng.model.Role;
 
+import java.util.Collections;
 import java.util.Set;
 
 public class LoggedUser {
+    private static LoggedUser LOGGED_USER = new LoggedUser();
     private int id;
-    private Set<Role> roles;
+    private Set<Role> roles = Collections.singleton(Role.USER);
     private boolean enabled;
 
-    public int getId() {
-        return id;
+    public static LoggedUser get() {
+        return LOGGED_USER;
+    }
+
+    public static int id(){
+        return get().id;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public boolean isEnabled() {
+        return enabled;
     }
 }
