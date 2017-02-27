c1b573242369709cf982351859e7005b1c711280
lWeRl <werasdas@gmail.com>
2017-02-27 21:17:16 +0300

    Add test data for meal

--- src/test/java/org/lwerl/caloriesmng/UserTestData.java
+++ src/test/java/org/lwerl/caloriesmng/UserTestData.java
@@ -2,6 +2,7 @@ package org.lwerl.caloriesmng;
 
 import org.lwerl.caloriesmng.model.Role;
 import org.lwerl.caloriesmng.model.User;
+import org.lwerl.caloriesmng.model.UserMeal;
 
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
@@ -12,4 +13,10 @@ import java.time.LocalDateTime;
 public class UserTestData {
     public static final User USER = new User(100000,"User","user@test.ru","user", Role.USER);
     public static final User ADMIN = new User(100001,"Admin","admin@test.ru","admin", Role.ADMIN);
+
+    public static final UserMeal MEAL1 = new UserMeal(100002,  "breakfast", LocalDateTime.of(2015, 1, 6, 9, 0),500);
+    public static final UserMeal MEAL2 = new UserMeal(100003, "dinner", LocalDateTime.of(2015, 1, 6, 13, 0), 1000);
+    public static final UserMeal MEAL3 = new UserMeal(100004, "supper", LocalDateTime.of(2015, 1, 7, 0, 0), 600);
+    public static final UserMeal MEAL4 = new UserMeal(100005,  "dinner", LocalDateTime.of(2015, 1, 7, 13, 0),1300);
+    public static final UserMeal ADMIN_MEAL = new UserMeal(100006, "admin_meal", LocalDateTime.of(2015, 1, 6, 14, 0), 2000);
 }
